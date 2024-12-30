2e7954b260f711bfde9edade83d47d3d2d29ab74
kyvc <kyvc.nta@gmail.com>
2024-12-30 08:37:14 +0700

    update

--- src/shared/configs/redis.config.ts
+++ src/shared/configs/redis.config.ts
@@ -1,21 +1,26 @@
 import Redis from 'ioredis';
 import { env } from '.';
 
-const connectRedisServer = () => {
-	const pubClient = new Redis({
-		port: env.redis.port,
-		host: env.redis.host,
-	});
-	const subClient = pubClient.duplicate();
-	pubClient.on('error', (err) => {
-		console.error('Redis Pub Client Error:', err);
-		process.exit(1);
-	});
+let pubClient: Redis | null = null;
+let subClient: Redis | null = null;
 
-	subClient.on('error', (err) => {
-		console.error('Redis Sub Client Error:', err);
-		process.exit(1);
-	});
+const connectRedisServer = () => {
+	if (!pubClient || !subClient) {
+		pubClient = new Redis({
+			port: env.redis.port,
+			host: env.redis.host,
+		});
+		subClient = pubClient.duplicate();
+		pubClient.on('error', (err) => {
+			console.error('Redis Pub Client Error:', err);
+			process.exit(1);
+		});
+
+		subClient.on('error', (err) => {
+			console.error('Redis Sub Client Error:', err);
+			process.exit(1);
+		});
+	}
 
 	return {
 		pubClient,
