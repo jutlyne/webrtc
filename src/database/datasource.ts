1158252e1cdde6f1956a0dbccf35e4e093694db7
kyvc <kyvc.nta@gmail.com>
2024-12-30 09:41:48 +0700

    update

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -1,8 +1,30 @@
 import path from 'path';
 import { env } from '@shared/configs';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { redisCacheInstance } from '@/shared/configs/redis.config';
 
 const config = env.database;
+
+const cacheConfig = () => {
+	if (env.app.node_env == 'testing') {
+		return {
+			type: 'database',
+			alwaysEnabled: true,
+			duration: 2000,
+			ignoreErrors: true,
+		};
+	}
+
+	const redisInstance = redisCacheInstance();
+
+	return {
+		type: 'ioredis',
+		options: redisInstance,
+		alwaysEnabled: true,
+		duration: 2000,
+		ignoreErrors: true,
+	};
+};
 
 export default new DataSource({
 	type: config.dialect,
@@ -16,18 +38,7 @@ export default new DataSource({
 	extra: {
 		insecureAuth: true,
 	},
-	cache: {
-		type: 'ioredis',
-		options: {
-			socket: {
-				host: env.redis_cache.host,
-				port: env.redis_cache.port,
-			},
-		},
-		alwaysEnabled: true,
-		duration: 2000,
-		ignoreErrors: true,
-	},
+	cache: cacheConfig,
 	entities: [path.join(__dirname, '../**/entities/*.entity.{ts,js}')],
 	migrations: [path.join(__dirname, '../database/migrations/*.{ts,js}')],
 	seeds: ['src/database/seeders/*{.ts,.js}'],
